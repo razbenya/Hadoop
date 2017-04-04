bb3c44dbb13a7436512ab9ced108fe1f230de8c6
Raz Ben Yaish <razb13@gmail.com>
2017-04-04 19:26:12 +0300

    Update StepThree.java

--- src/Main/StepThree.java
+++ src/Main/StepThree.java
@@ -20,7 +20,7 @@ import org.apache.hadoop.util.ToolRunner;
 import java.io.IOException;
 
 /**
- * Created by Neta on 2017-03-08.
+ * Created by Razb on 2017-03-08.
  */
 public class StepThree extends Configured implements Tool {
     private static final String inputPath = "s3n://razbucket2/TempOutPut2";
